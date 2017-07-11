f0d3649cc6785e067a5e643388043b67d2fec58c
cperez2187 <cperez2187@gmail.com>
2017-07-11 10:01:21 -0700

    Fucntionality complete. Updates database and displays receipt

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,24 +12,110 @@ const connection = mysql.createConnection({
 	database: 'bamazon_DB'
 });
 
-// Make the connection to the database
+// Make the connection to the database and engage with the customer
 connection.connect((err) => {
 	if (err) throw err;
 
 	console.log(`Connected as id ${connection.threadId}`);
 
-	displayProducts();
-});
-
-function displayProducts() {
-
 	connection.query('SELECT * FROM products', (error, results) => {
 		if (error) throw error;
 
+		// Display products in table form
 		productsTable(results);
+
+		buyProduct(results);
+	});
+	
+});
+
+
+function buyProduct(products) {
+
+	inquirer
+	  .prompt([
+	    // Here we create a basic text prompt.
+	    {
+	      type: "input",
+	      name: "id",
+	      message: "What is the ID of the product you would like to buy?",
+	      validate: (value) => {
+	      	if (value <= products.length) {
+	      		return true;
+	      	}
+	      	return false;
+	      }
+	    },
+	    {
+	      type: "input",
+	      name: "units",
+	      message: "How many units of the product would you like to buy?",
+	      validate: (value) => {
+	      	if (value > 0) {
+	      		return true;
+	      	}
+	      	return false;
+	      }
+	    }
+	])
+	.then((answer) => {
+
+		let productId = answer.id;
+		let unitsRequested = answer.units;
+
+		// Retrieve product name, price and stock quantity
+		let query = 'SELECT product_name, price, stock_quantity FROM products WHERE id=?';
+		connection.query(query, [answer.id], (error, results) => {
+			if (error) throw error;
+
+			let productName = results[0].product_name;
+			let productPrice = results[0].price;
+			let stockQuantity = results[0].stock_quantity;
+			// console.log(results);
+			
+			// console.log(`ID: ${productId} Units: ${unitsRequested} Quantity: ${stockQuantity}`);
+
+			if (unitsRequested <= stockQuantity) {
+				updateProducts(productId, unitsRequested, productName, productPrice, stockQuantity);
+			} else {
+				throw 'Insufficient quantity';
+			}
+			
+		});
 	});
 }
 
+
+// Update database when purchase is made
+function updateProducts(id, units, name, price, stockQty) {
+	// console.log(`ID: ${id} Units: ${units}`);
+
+	let query = 'UPDATE products SET ? WHERE ?';
+
+	connection.query(query,
+	[
+		{
+			stock_quantity: stockQty - units
+		},
+		{
+			id: id
+		}
+	], (error, results) => {
+		if (error) throw error;
+
+		// Show customer their purchase
+		purchaseTable = new Table({
+			head: ['Product', 'Units', 'Total Cost']
+		});
+
+		purchaseTable.push([name, `${units}X`, `$${units*price}`]);
+		console.log('\nThank you for your purchase, here is your receipt...');
+		console.log(purchaseTable.toString());
+	});
+}
+
+
+// Creates a table to display products
 function productsTable(results) {
 
 	// Create table with headers
